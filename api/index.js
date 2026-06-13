d1bfcac5ffd67d809e27e8fdd1e3616c6dab8e3d
Abdullah <abdullahmerdin@gmail.com>
2026-06-13 18:49:38 +0300

    Prisma binaryTargets for Vercel + handler fix

--- api/index.js
+++ api/index.js
@@ -1,11 +1,7 @@
 import { createRequestHandler } from "@react-router/express";
-import { installGlobals } from "@react-router/node";
-import * as build from "../build/server/index.js";
-
-installGlobals();
 
 const handler = createRequestHandler({
-  build,
+  build: await import("../build/server/index.js"),
   mode: process.env.NODE_ENV,
 });
 
